84fd84648e622f50f386b033d1feb600cded5b4d
hassan-theitguy <hassan.voyeau@gmail.com>
2023-06-07 12:41:42 -0400

    made some changes

--- poems/build.sh
+++ poems/build.sh
@@ -4,9 +4,22 @@ else
   export NODE_ENV_POEMS="production"
 fi
 
+rm -rf ../docs
+
 npx @11ty/eleventy
 
+pf_dir="/data/data/com.termux/files/home/.cargo/bin"
+
+if [[ ":$PATH:" != *":$pf_dir:"* ]]; then
+    echo "Pagefind dir is not in PATH. Adding it."
+    export PATH="$pf_dir:$PATH"
+else
+    echo "Pagefind dir is already in PATH."
+fi
+
 pagefind --source "../docs/poems" --glob "**/*.*"
+
+touch ../docs/.nojekyll
 
 echo $NODE_ENV_POEMS
 echo $1
